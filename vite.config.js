2f23944e81cbd88b43b99cafd1c3c850d485759b
ollie <behoya11@gmail.com>
2024-12-08 14:00:31 +0800

    chore: 修改 vite config、index.html、admin.html 放置 src/

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { resolve } from "path";
 
 export default {
+  root: "src", // 設置 src 為開發環境的根目錄
   base: "/2024-hex-js-week08-mainTask/",
   build: {
-    outDir: "./dist",
+    outDir: "../dist", // 設定輸出目錄相對於根目錄
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+        admin: "src/admin.html", // 可選，若有其他頁面
+      },
+    },
   },
 };
